91d5ba423746bffe166f691580e61d245f407928
Patrick Styll <patrick.styll@tuwien.ac.at>
2025-01-14 23:35:34 +0100

    update decoder argument type to string in lightning classifier

--- src/module/pl_classifier.py
+++ src/module/pl_classifier.py
@@ -460,6 +460,6 @@ class LitClassifier(pl.LightningModule):
         
         # decoder related
         group.add_argument("--num_classes", type=int, default=2, help="Number of distinct target classes")
-        group.add_argument("--decoder", type=int, default=1, help="Which decoder to use: (i) single_target_decoder - predict a single value via regression or classification | (ii) series_decoder: predict a series of values (one per timeframe) via regression")
+        group.add_argument("--decoder", type=str, default="single_target_decoder", help="Which decoder to use: (i) single_target_decoder - predict a single value via regression or classification | (ii) series_decoder: predict a series of values (one per timeframe) via regression")
         
         return parser
