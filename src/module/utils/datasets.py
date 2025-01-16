4002d709788d208bef11a083be01cc3f4b7b46bc
Jubin <jubin@gpu-1.novalocal>
2025-01-16 21:53:19 +0900

    add r2score, add --adjust_hrf option in data_module, datasets.py

--- src/module/utils/datasets.py
+++ src/module/utils/datasets.py
@@ -1,3 +1,4 @@
+
 # 4D_fMRI_Transformer
 import os
 import torch
@@ -7,6 +8,8 @@ from torch.utils.data import Dataset
 import numpy as np
 import torchio as tio
 import random
+import glob
+import re
 
 class BaseDataset(Dataset):
     def __init__(self, **kwargs):
@@ -44,7 +47,7 @@ class BaseDataset(Dataset):
                 
         for fname in load_fnames:
             img_path = os.path.join(subject_path, fname)
-            y_i = torch.load(img_path).unsqueeze(0)
+            y_i = torch.load(img_path, weights_only=True).unsqueeze(0) # weights only (?)
             y.append(y_i)
         y = torch.cat(y, dim=4)
         return y
@@ -57,6 +60,82 @@ class BaseDataset(Dataset):
 
     def _set_data(self, root, subject_dict):
         raise NotImplementedError("Required function")
+
+class HBN(BaseDataset):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.decoder = kwargs.get('decoder')
+        print(f"Number of sequences: {len(self.data)}")
+        self.count_unique_subjects()
+    
+    def count_unique_subjects(self):
+        unique_subjects = set([tup[1] for tup in self.data])
+        print(f"Number of unique subjects: {len(unique_subjects)}")
+
+    def _set_data(self, root, subject_dict):
+        if self.adjust_hrf:
+            start_TR = 7
+        else:
+            start_TR = 0
+        data = []
+        
+        img_root = os.path.join(root, 'img') 
+
+        for i, subject_name in enumerate(subject_dict):
+            sex, target = subject_dict[subject_name]
+            subject_path = os.path.join(img_root, subject_name)
+            num_frames = len([ # exclude e.g. frame_seq-10_610_620.pt
+                f for f in glob.glob(os.path.join(subject_path, 'frame_*.pt'))
+                if re.search(r'frame_\d+\.pt$', os.path.basename(f))
+            ])
+            session_duration = num_frames - self.sample_duration + 1 - start_TR
+            
+            for start_frame in range(start_TR, session_duration, self.stride):
+                if self.decoder == 'series_decoder':
+                    data_tuple = (i,
+                                subject_name,
+                                subject_path,
+                                start_frame,
+                                self.sample_duration,
+                                num_frames,
+                                target[start_frame:min(start_frame+self.sample_duration,num_frames)],
+                                sex)
+                elif self.decoder == 'single_target_decoder':
+                    data_tuple = (i,
+                                  subject_name,
+                                  subject_path,
+                                  start_frame,
+                                  self.stride,
+                                  num_frames,
+                                  target,
+                                  sex)
+                else:
+                    raise ValueError("Invalid decoder")
+                data.append(data_tuple)
+
+        # train dataset
+        # for regression tasks
+        if self.train: 
+            self.target_values = np.array([tup[6] for tup in data]).reshape(-1, 1)
+
+        return data
+
+    def __getitem__(self, index):
+        _, subject_name, subject_path, start_frame, sequence_length, num_frames, target, sex = self.data[index]
+        y = self.load_sequence(subject_path, start_frame, sequence_length, num_frames)
+
+        background_value = y.flatten()[0]
+        y = y.permute(0,4,1,2,3) 
+        y = torch.nn.functional.pad(y, (7, 8, 0, 1, 7, 8), value=background_value) # adjust this padding level according to your data 
+        y = y.permute(0,2,3,4,1) 
+
+        return {
+                    "fmri_sequence": y,
+                    "subject_name": subject_name,
+                    "target": target,
+                    "TR": start_frame,
+                    "sex": sex,
+                } 
     
 class Dummy(BaseDataset):
     def __init__(self, **kwargs):
